e3d2f44eeddca7d2f6508eabd6f29ca9872a97c0
Michael Manning <michael@manning390.com>
2022-07-01 16:39:23 -0700

    Actually filled Dialogue Model out

--- app/Models/Dialogue.php
+++ app/Models/Dialogue.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use League\CommonMark\CommonMarkConverter;
 
 class Dialogue extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['speaker', 'line', 'order'];
+
+    public function report() {
+        return $this->belongsTo(Report::class);
+    }
+
+    /**
+     * Get the html of the line in markdown format
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    public function html(): Attribute {
+        $converter = new CommonMarkConverter();
+        return Attribute::make(
+            get: fn ($value, $attributes) => $converter->convert($attributes['line'])
+        );
+    }
+
+
 }
