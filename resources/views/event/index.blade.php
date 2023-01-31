ca77ff3fe0792bf43a66c9adb1ef89464bc8a8ae
Michael Manning <michael@manning390.com>
2023-01-30 20:17:02 -0800

    Quick styles for events index

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,8 +1,14 @@
-Events Index
-<br />
-@foreach($events as $event)
-    <p>{{ $event->name }}</p>
-    <p>{{ $event->date }}</p>
-    <p>{{ $event->location }}</p>
-    <hr />
-@endforeach
+<x-app-layout>
+    <h2 class="mb-4 text-5xl bg-midnight p-4">Events</h2>
+    <div class="container mx-auto">
+        <div class="flex flex-col gap-5">
+            @foreach($events as $event)
+                <section class="border rounded border-gray-200 p-4">
+                    <a href="{{ route('event.show', $event) }}">{{ $event->name }}</a>
+                    <p>{{ $event->date->diffForHumans() }}</p>
+                    <p>{{ $event->location }}</p>
+                </section>
+            @endforeach
+        </div>
+    </div>
+</x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,4 @@
-{{-- @include('gnome') --}}
+@include('gnome')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
