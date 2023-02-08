e4cd1c7ffdcc869193a3439930f74c7899c872e6
tkomarlu <Tanay>
2023-02-07 22:07:45 -0800

    Finished home page background

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Event;
 use App\Models\Report;
+use App\Inspiring;
 
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,22 +17,15 @@ class HomeController extends Controller
             ->orderBy('date', 'desc')
             ->take(5)
             ->get();
-
-        return Inertia::render('Home', compact('events'));
+        $quote = Inspiring::quote();
+        return Inertia::render('Home', compact('events', 'quote'));
     }
 
     public function search(Request $request): Response {
         $reports = Report::search(
             query: trim($request->input('q'))
-            // query: trim($request->input('q')),
-            // callback: function(Indexes $meilisearch, string $query, array $options) use ($request) {
-                // if ($request->has(key: 'tags')) {
-                    // $options['filter'] = "tags = {$request->get('tags')}";
-                // }
-                // return $meilisearch->search($query, $options);
-            // }
         );
-        return Inertia::render('search', compact('reports'));
+        return Inertia::render('Search', compact('reports'));
     }
 
 }
