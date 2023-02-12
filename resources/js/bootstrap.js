35f87d53ad8a7932fd470adfb38880efeb625936
Michael Manning <michael@manning390.com>
2023-02-11 16:47:38 -0800

    Added helper for inertia

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,8 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.dumpData = () => console.log(JSON.parse(document.getElementById('app').getAttribute('data-page')));
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
