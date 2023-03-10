0478aadb254a25a70d976e61a640381c9b8af8c3
Michael Manning <michael@manning390.com>
2023-03-10 15:34:15 -0800

    Initial setup for Eslint and Prettier (#28)
    * Added Eslint and Prettier configurations
    
    * Ran the lint and formatters
    
    * Added githooks
    
    * Added ignore warnings for ziggy routes
    
    * Added .eslintcache to .gitignore

--- resources/js/Pages/Report/Show.svelte
+++ resources/js/Pages/Report/Show.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <svelte:head>
-    <meta property="og:title" content={`#${report.id} | Abidan Archive`}>
-    <meta property="og:type" content="article">
-    <meta property="og:url"  content={report.permalink}>
-    <meta property="og:site_name" content="Abidan Archive">
-    <meta property="og:description" content={report.dialogues[0].line}>
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:title" content={`#${report.id} | Abidan Archive`}>
-    <meta name="twitter:description" content={report.dialogues[0].line}>
+    <meta property="og:title" content={`#${report.id} | Abidan Archive`} />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content={report.permalink} />
+    <meta property="og:site_name" content="Abidan Archive" />
+    <meta property="og:description" content={report.dialogues[0].line} />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content={`#${report.id} | Abidan Archive`} />
+    <meta name="twitter:description" content={report.dialogues[0].line} />
 </svelte:head>
 
 <section class="container mx-auto p-5">
