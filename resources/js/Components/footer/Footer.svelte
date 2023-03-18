756ad9829208a437d8389e3f6338b66ea3c3d0dd
tkomarlu <Tanay>
2023-03-18 15:10:27 -0700

    Fixed mobile styling for events

--- resources/js/Components/footer/Footer.svelte
+++ resources/js/Components/footer/Footer.svelte
@@ -6,6 +6,7 @@
         Instagram,
         Twitter,
         Github,
+        Gnome,
     } from '@components/icons';
     const links = {
         facebook: 'https://www.facebook.com/willwightauthor',
@@ -13,15 +14,30 @@
         reddit: 'https://www.reddit.com/r/Iteration110Cradle',
         github: 'https://github.com/Abidan-Archive/archive',
         instagram: 'https://www.instagram.com/willwight110',
+        willBlog: 'https://www.willwight.com',
     };
 </script>
 
 <footer {...$$restProps} class={`bg-gray-800 p-4 sm:p-6 ${$$props.class}`}>
-    <div class="sm:flex sm:items-center sm:justify-between">
+    <div class="sm:mx-auto sm:flex sm:items-center sm:justify-between">
         <span class="block text-sm text-gray-400 sm:text-center">
             Made and maintained by Telariel, the Spider
         </span>
         <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            <FooterIcon
+                href={links['willBlog']}
+                target="_blank"
+                rel="noreferrer"
+                class="text-gray-400  hover:text-abidan-400">
+                <Gnome />
+            </FooterIcon>
+            <FooterIcon
+                href={links['reddit']}
+                target="_blank"
+                rel="noreferrer"
+                class="text-gray-400  hover:text-abidan-400">
+                <Reddit />
+            </FooterIcon>
             <FooterIcon
                 href={links['facebook']}
                 target="_blank"
@@ -42,13 +58,6 @@
                 rel="noreferrer"
                 class="text-gray-400  hover:text-abidan-400">
                 <Twitter />
-            </FooterIcon>
-            <FooterIcon
-                href={links['reddit']}
-                target="_blank"
-                rel="noreferrer"
-                class="text-gray-400  hover:text-abidan-400">
-                <Reddit />
             </FooterIcon>
             <FooterIcon
                 href={links['github']}
