9b46549f790cfa2e10ab6db32c2ee4eef34a3b06
Michael Manning <michael@manning390.com>
2023-03-18 13:08:28 -0700

    hotfix on searchform import

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -1,7 +1,7 @@
 <script>
     import { inertia } from '@inertiajs/svelte';
     import Star from '@components/Star.svelte';
-    import Searchform from '@components/Searchform.svelte';
+    import SearchForm from '@components/SearchForm.svelte';
     import route from '@/route';
 
     export let events;
@@ -18,7 +18,7 @@
                 {quote}
             </section>
             <!-- Search -->
-            <Searchform />
+            <SearchForm />
         </div>
     </Star>
 </section>
