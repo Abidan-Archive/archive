55f76e088b402d9ab7e4746923d438a3a87b9c6f
Tanay Komarlu <54658599+tkomarlu@users.noreply.github.com>
2023-03-24 11:35:58 -0700

    Adds footer to layout (#30)
    * Add footer with socials
    
    * Run formatter
    
    * fixed hover styles
    
    * Fixed mobile styling for events
    
    * removed instagram
    
    * removed instagram reference
    
    * Adjusted footer components to be a little cleaner
    
    ---------
    
    Co-authored-by: tkomarlu <Tanay>
    Co-authored-by: Michael Manning <michael@manning390.com>

---
+++ resources/js/Components/footer/Footer.svelte
@@ -0,0 +1,43 @@
+<script>
+    import FooterIcon from '@components/Footer/FooterIcon.svelte';
+    import {
+        Reddit,
+        Facebook,
+        Twitter,
+        Github,
+        Gnome,
+    } from '@components/icons';
+    const links = [
+        {
+            icon: Gnome,
+            href: 'https://www.willwight.com',
+        },
+        {
+            icon: Reddit,
+            href: 'https://www.reddit.com/r/Iteration110Cradle',
+        },
+        {
+            icon: Facebook,
+            href: 'https://www.facebook.com/willwightauthor',
+        },
+        {
+            icon: Twitter,
+            href: 'https://twitter.com/williamwight',
+        },
+    ];
+</script>
+
+<footer {...$$restProps} class={`bg-gray-800 p-4 sm:p-6 ${$$props.class}`}>
+    <div class="sm:mx-auto sm:flex sm:items-center sm:justify-between">
+        <div class="text-sm text-gray-400 sm:text-center">
+            This site is built and maintained by the Will Wight fanbase and community.
+        </div>
+        <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+            {#each links as link}
+                <FooterIcon href={link.href}>
+                    <svelte:component this={link.icon} />
+                </FooterIcon>
+            {/each}
+        </div>
+    </div>
+</footer>
---
+++ resources/js/Components/footer/FooterIcon.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let href = '';
+    export let ariaLabel = '';
+    export let target = '_blank';
+    export let ref = 'noreferrer';
+</script>
+
+{#if href}
+    <a
+        {...$$restProps}
+        {href}
+        {target}
+        {ref}
+        aria-label={ariaLabel}
+        class={`text-gray-400  hover:text-abidan-400 ${$$props.class}`}>
+        <slot />
+    </a>
+{:else}
+    <slot />
+{/if}
