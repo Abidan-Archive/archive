0f5ba247bacb44f02e5775ae7f4704ca6d9ed7a7
Michael Manning <michael@manning390.com>
2023-03-24 21:44:05 -0700

    Adjust colors of paginator

--- resources/js/Components/Paginator.svelte
+++ resources/js/Components/Paginator.svelte
@@ -26,14 +26,14 @@
         <div class="flex flex-1 justify-between sm:hidden">
             {#if current_page === 0}
                 <span
-                    class="relative inline-flex cursor-default items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-500">
+                    class="border-muenkai relative inline-flex cursor-default items-center rounded-md border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-100">
                     {@html prev_page_label}
                 </span>
             {:else}
                 <a
                     use:inertia
                     href={prev_page_url}
-                    class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-700 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-500 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-700"
+                    class="border-muenkai relative inline-flex items-center rounded-md border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-300 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-500 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-100"
                     >{@html prev_page_label}</a>
             {/if}
 
@@ -41,11 +41,11 @@
                 <a
                     use:inertia
                     href={next_page_url}
-                    class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-700 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-500 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-700"
+                    class="border-muenkai relative ml-3 inline-flex items-center rounded-md border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-300 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-500 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-300"
                     >{@html next_page_label}</a>
             {:else}
                 <span
-                    class="relative ml-3 inline-flex cursor-default items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-500"
+                    class="border-muenkai relative ml-3 inline-flex cursor-default items-center rounded-md border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-500"
                     >{@html next_page_label}</span>
             {/if}
         </div>
@@ -53,7 +53,7 @@
         <div
             class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div>
-                <p class="text-sm leading-5 text-gray-700">
+                <p class="text-sm leading-5 text-gray-300">
                     Showing
                     {#if current_page === 0}
                         <span class="font-medium">{from}</span>
@@ -73,7 +73,7 @@
                     {#if current_page === 0}
                         <span aria-disabled="true" aria-label={prev_page_label}>
                             <span
-                                class="relative inline-flex cursor-default items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium leading-5 text-gray-500"
+                                class="border-muenkai relative inline-flex cursor-default items-center rounded-l-md border bg-midnight-700 px-2 py-2 text-sm font-medium leading-5 text-gray-500"
                                 aria-hidden="true">
                                 <Previous />
                             </span>
@@ -84,7 +84,7 @@
                             href={prev_page_url}
                             rel="prev"
                             aria-label={prev_page_label}
-                            class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium leading-5 text-gray-500 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-400 focus:z-10 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-500">
+                            class="border-muenkai relative inline-flex items-center rounded-l-md border bg-midnight-700 px-2 py-2 text-sm font-medium leading-5 text-gray-500 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-400 focus:z-10 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-500">
                             <Previous />
                         </a>
                     {/if}
@@ -93,13 +93,13 @@
                         {#if link.label === '...'}
                             <span aria-disabled="true">
                                 <span
-                                    class="relative -ml-px inline-flex cursor-default items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-700"
+                                    class="border-muenkai relative -ml-px inline-flex cursor-default items-center border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-300"
                                     >...</span>
                             </span>
                         {:else if link.active}
                             <span aria-current="page">
                                 <span
-                                    class="relative -ml-px inline-flex cursor-default items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-500"
+                                    class="border-muenkai relative -ml-px inline-flex cursor-default items-center border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-500"
                                     >{@html link.label}</span>
                             </span>
                         {:else}
@@ -107,7 +107,7 @@
                                 use:inertia
                                 href={link.url}
                                 aria-label={`Go to page ${link.label}`}
-                                class="relative -ml-px inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium leading-5 text-gray-700 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-500 focus:z-10 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-700">
+                                class="border-muenkai relative -ml-px inline-flex items-center border bg-midnight-700 px-4 py-2 text-sm font-medium leading-5 text-gray-300 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-500 focus:z-10 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-300">
                                 {@html link.label}
                             </a>
                         {/if}
@@ -120,13 +120,13 @@
                             href={next_page_url}
                             rel="next"
                             aria-label={next_page_label}
-                            class="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium leading-5 text-gray-500 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-400 focus:z-10 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-100 active:text-gray-500">
+                            class="border-muenkai relative -ml-px inline-flex items-center rounded-r-md border bg-midnight-700 px-2 py-2 text-sm font-medium leading-5 text-gray-200 ring-gray-300 transition duration-150 ease-in-out hover:text-gray-400 focus:z-10 focus:border-blue-300 focus:outline-none focus:ring active:bg-gray-500 active:text-gray-100">
                             <Next />
                         </a>
                     {:else}
                         <span aria-disabled="true" aria-label={next_page_label}>
                             <span
-                                class="relative -ml-px inline-flex cursor-default items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium leading-5 text-gray-500"
+                                class="border-muenkai relative -ml-px inline-flex cursor-default items-center rounded-r-md border bg-midnight-700 px-2 py-2 text-sm font-medium leading-5 text-gray-100"
                                 aria-hidden="true">
                                 <Next />
                             </span>
