8fa86e026e9c5e1fd5ff959821b3d6d5bfa50cd8
Michael Manning <michael@manning390.com>
2023-03-27 15:47:08 -0700

    Added new Form Components

---
+++ resources/js/Components/forms/ErrorMessage.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let message;
+</script>
+
+{#if !!message}
+    <ul class={`space-y-1 text-sm text-red-400 ${$$props.class}`}>
+        <li>{message}</li>
+    </ul>
+{/if}
---
+++ resources/js/Components/forms/Label.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let value;
+
+    let className;
+    export { className as class };
+</script>
+
+<label
+    class={`block text-sm font-medium text-gray-300 ${className}`}
+    {...$$restProps}>
+    {#if !!value}
+        {value}
+    {:else}
+        <slot />
+    {/if}
+</label>
