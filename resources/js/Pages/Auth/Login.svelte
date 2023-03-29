14d7da9c95fdce29b69d433b4070dd00d912f09d
Michael Manning <michael@manning390.com>
2023-03-29 13:59:46 -0700

    Create Basic Auth Pages (#35)
    * Moved gnome to prod only
    
    * Added new Form Components
    
    * Changed name to username for Users Model
    
    * Created initial registration form
    
    * Created initial login form
    
    * Created initial verify email get page
    
    * Created initial Forgot Password page
    
    * Create confirm password page
    
    * Created reset password page
    
    * Removed unused imports etc.
    
    * Fix loose ends

---
+++ resources/js/Pages/Auth/Login.svelte
@@ -0,0 +1,85 @@
+<script>
+    import Status from '@components/Status.svelte';
+    import clsx from 'clsx';
+    import route from '@/route';
+    import {
+        ErrorMessage,
+        Label,
+        PrimaryButton,
+        TextInput,
+    } from '@components/forms';
+    import { useForm, inertia } from '@inertiajs/svelte';
+
+    export let status;
+
+    let form = useForm({
+        email: null,
+        password: null,
+        remember: false,
+    });
+
+    function submit() {
+        $form.post(route('login'));
+    }
+</script>
+
+<Status {status} />
+
+<form method="POST" on:submit|preventDefault={submit}>
+    <div>
+        <Label for="email">Email</Label>
+        <!-- svelte-ignore a11y-autofocus -->
+        <TextInput
+            id="email"
+            name="email"
+            bind:value={$form.email}
+            class="mt-1 block w-full"
+            required
+            autofocus />
+        <ErrorMessage message={$form.errors.email} class="mt-2" />
+    </div>
+
+    <div class="mt">
+        <Label for="password">Password</Label>
+        <TextInput
+            id="password"
+            type="password"
+            name="password"
+            bind:value={$form.password}
+            class="mt-1 block w-full"
+            required
+            autocomplete="current-password" />
+        <ErrorMessage message={$form.errors.password} class="mt-2" />
+    </div>
+
+    <div class="mt-4 block">
+        <Label for="remember_me" class="inline-flex items-center">
+            <input
+                id="remember_me"
+                type="checkbox"
+                class={clsx(
+                    'rounded border-gray-700 bg-gray-900 text-indigo-600 shadow-sm',
+                    'focus:ring-indigo-600 focus:ring-offset-gray-800'
+                )}
+                bind:checked={$form.remember}
+                name="remember" />
+            <span class="ml-2 text-sm text-gray-400">Remember Me</span>
+        </Label>
+    </div>
+
+    <div class="mt-4 flex items-center justify-end">
+        {#if route().has('password.request')}
+            <a
+                use:inertia
+                href={route('password.request')}
+                class={clsx(
+                    'rounded-md text-sm text-gray-400 underline hover:text-gray-100',
+                    'focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800'
+                )}>
+                Forgot your password?
+            </a>
+        {/if}
+
+        <PrimaryButton class="ml-3">Log in</PrimaryButton>
+    </div>
+</form>
---
+++ resources/js/Pages/Auth/Register.svelte
@@ -0,0 +1,97 @@
+<script>
+    import clsx from 'clsx';
+    import route from '@/route';
+    import {
+        ErrorMessage,
+        Label,
+        PrimaryButton,
+        TextInput,
+    } from '@components/forms';
+    import { useForm, inertia } from '@inertiajs/svelte';
+
+    let form = useForm('Register', {
+        username: null,
+        email: null,
+        email_confirmation: null,
+        password: null,
+        password_confirmation: null,
+    });
+
+    function submit() {
+        $form.post(route('register'));
+    }
+</script>
+
+<form method="POST" on:submit|preventDefault={submit}>
+    <div>
+        <Label for="username" value="Username" />
+        <TextInput
+            id="username"
+            class="mt-1 block w-full"
+            name="username"
+            bind:value={$form.username}
+            required
+            autofocus />
+        <ErrorMessage message={$form.errors.username} class="mt-2" />
+    </div>
+    <div class="mt-4">
+        <Label for="email" value="Email" />
+        <TextInput
+            id="email"
+            class="mt-1 block w-full"
+            type="email"
+            name="email"
+            bind:value={$form.email}
+            required />
+        <ErrorMessage message={$form.errors.email} class="mt-2" />
+    </div>
+    <div class="mt-4">
+        <Label for="email_confirmation" value="Confirm Email" />
+        <TextInput
+            id="email_confirmation"
+            class="mt-1 block w-full"
+            type="email"
+            name="email_confirmation"
+            bind:value={$form.email_confirmation}
+            required />
+        <ErrorMessage message={$form.errors.email_confirmation} class="mt-2" />
+    </div>
+    <div class="mt-4">
+        <Label for="password" value="Password" />
+        <TextInput
+            id="password"
+            class="mt-1 block w-full"
+            type="password"
+            name="password"
+            bind:value={$form.password}
+            required
+            autocomplete="new-password" />
+        <ErrorMessage message={$form.errors.password} class="mt-2" />
+    </div>
+    <div class="mt-4">
+        <Label for="password_confirmation" value="Confirm Password" />
+        <TextInput
+            id="password_confirmation"
+            class="mt-1 block w-full"
+            type="password"
+            name="password_confirmation"
+            bind:value={$form.password_confirmation}
+            required />
+        <ErrorMessage
+            message={$form.errors.password_confirmation}
+            class="mt-2" />
+    </div>
+    <div class="mt-4 flex items-center justify-end">
+        <a
+            use:inertia
+            href={route('login')}
+            class={clsx(
+                'rounded-md text-sm text-gray-400 underline hover:text-gray-100',
+                'focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800'
+            )}>
+            Already registered?
+        </a>
+
+        <PrimaryButton class="ml-4">Register</PrimaryButton>
+    </div>
+</form>
