7564ba084fa245a78cdaff12230190c7d0dfe9a0
tkomarlu <Tanay>
2023-04-14 00:42:58 -0700

    Created event index page

--- resources/js/Pages/Event/Index.svelte
+++ resources/js/Pages/Event/Index.svelte
@@ -1,26 +1,32 @@
 <script>
-    import { inertia } from '@inertiajs/svelte';
-    import route from '@/route';
+    import Paginator from '@components/Paginator.svelte';
+    import Event from '@components/Event.svelte';
+    import PaginatorSimple from '@components/PaginatorSimple.svelte';
     export let events;
+    export let paginate;
+    console.log(events);
 </script>
 
 <svelte:head>
     <title>Events | Abidan Archive</title>
 </svelte:head>
 
-<section class="container mx-auto pt-5">
-    <h2 class="my-5 text-2xl">Events</h2>
+<div class="container mx-auto pt-5">
+    <section>
+        <h2 class="my-5 text-3xl">Events</h2>
+    </section>
     <hr class="my-5" />
-    <div>
-        {#each events as event}
-            <a
-                class="hover:underline"
-                use:inertia
-                href={route('event.show', event)}>
-                <article>
-                    {event.name}
-                </article>
-            </a>
-        {/each}
-    </div>
-</section>
+    <section class="flex flex-col gap-3">
+        {#if !events.length}
+            <div class="text-center">
+                <p>No Events found.</p>
+            </div>
+        {:else}
+            <Paginator {...paginate} />
+            {#each events as event}
+                <Event {event} />
+            {/each}
+            <Paginator {...paginate} />
+        {/if}
+    </section>
+</div>
