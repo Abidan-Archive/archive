8d51398e7216422c45023cdfc2fff536f0a49350
Michael Manning <michael@manning390.com>
2024-01-12 21:34:39 -0800

    Adding a login button

--- resources/js/Components/Navigation.svelte
+++ resources/js/Components/Navigation.svelte
@@ -7,9 +7,13 @@
         Calendar,
         Discord,
         Hamburger,
+        User,
+        XMark,
     } from '@/Components/icons';
     import route from '@/Utils/route';
 
+    // External links through use:inertia feels bad, see if we can get a change
+    // https://github.com/inertiajs/inertia/issues/1719#issuecomment-1890295472
     const links = [
         $page.props.auth.user?.roles.includes('admin')
             ? {
@@ -27,23 +31,29 @@
             label: 'Discord',
             href: 'https://discord.gg/tCg94qy',
             icon: Discord,
+            external: true,
         },
         {
             label: 'Wiki',
             href: 'https://wiki.abidanarchive.com',
             icon: Globe,
+            external: true,
         },
+        !$page.props.auth.user
+            ? {
+                  label: 'Login',
+                  href: route('login'),
+                  icon: User,
+              }
+            : null,
     ].filter(Boolean);
 
     let showMenu = false;
-
-    function toggleNavbar() {
-        showMenu = !showMenu;
-    }
+    let showProfileMenu = false;
 </script>
 
 <nav
-    class="fixed top-0 left-0 z-50 h-16 w-full border-l-5 border-primary-400 bg-gray-800">
+    class="fixed top-0 left-0 z-40 h-16 w-full border-l-5 border-primary-400 bg-gray-800">
     <div
         class="mx-6 px-4 py-2 md:flex md:items-center md:justify-between md:px-0 md:py-1">
         <div class="flex items-center justify-between">
@@ -55,11 +65,11 @@
             </a>
             <!-- Mobile menu button -->
             <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <div on:click={toggleNavbar} class="flex md:hidden">
+            <div on:click={() => (showMenu = !showMenu)} class="flex md:hidden">
                 <button
                     type="button"
                     class="rounded-lg bg-gray-800 p-2 text-white hover:bg-gray-700 focus:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600 active:bg-gray-800 md:hidden">
-                    <Hamburger />
+                    <svelte:component this={!showMenu ? Hamburger : XMark} />
                 </button>
             </div>
         </div>
@@ -69,15 +79,35 @@
             class="mt-6 flex-col space-y-6 rounded bg-gray-800 py-4 px-8 sm:mt-4 sm:border sm:border-gray-600 md:mt-0 md:flex md:flex-row md:items-center md:space-y-0 md:space-x-6 md:border-0 lg:space-x-10"
             class:hidden={!showMenu}
             class:flex={showMenu}>
-            {#each links as { label, href, icon }}
-                <a
-                    class="text-white-500 flex hover:text-primary-300 "
-                    use:inertia
-                    {href}>
-                    <svelte:component this={icon} class="mx-2 h-6 w-6" />
-                    {label}
-                </a>
+            {#each links as { label, href, icon, external = false }}
+                {#if !external}
+                    <a
+                        class="text-white-500 flex hover:text-primary-300 "
+                        use:inertia
+                        {href}>
+                        <svelte:component this={icon} class="mx-2 h-6 w-6" />
+                        {label}
+                    </a>
+                {:else}
+                    <a
+                        class="text-white-500 flex hover:text-primary-300 "
+                        {href}>
+                        <svelte:component this={icon} class="mx-2 h-6 w-6" />
+                        {label}
+                    </a>
+                {/if}
             {/each}
+            {#if !!$page.props.auth.user}
+                <button
+                    class="text-white-500 flex hover:text-primary-300"
+                    on:click={() => {
+                        showMenu = false;
+                        showProfileMenu = true;
+                    }}>
+                    <User class="mx-2 h-6 w-6" />
+                    {$page.props.auth.user?.email}
+                </button>
+            {/if}
         </div>
     </div>
 </nav>
