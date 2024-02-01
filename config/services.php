e64d5dcd027ee2b26c2912817a4a5be29e2be994
Michael Manning <michael@manning390.com>
2024-02-01 14:03:56 -0800

    Make redirect uri dynamic

--- config/services.php
+++ config/services.php
@@ -39,7 +39,7 @@ return [
     'discord' => [
         'client_id' => env('DISCORD_CLIENT_ID'),
         'client_secret' => env('DISCORD_CLIENT_SECRET'),
-        'redirect' => env('DISCORD_REDIRECT_URI'),
+        'redirect' => url('oauth/callback/discord'),
     ],
 
 ];
