6786b2ddba1149711a823ca88d9fbede1f14b6ba
Shift <shift@laravelshift.com>
2024-07-08 03:17:24 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,29 @@
 
 namespace App\Providers;
 
+use App\Models\Permission;
+use App\Models\User;
+use App\Policies\LikePolicy;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to the "home" route for your application.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/';
+
     /**
      * Register any application services.
      */
@@ -20,5 +39,47 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+
+        $this->bootAuth();
+        $this->bootRoute();
+    }
+
+    public function bootAuth(): void
+    {
+
+        if (! $this->app->environment('deployment') && Schema::hasTable('permissions')) {
+            // Get all the permissions
+            $permissions = Permission::with('roles')->get();
+            // Dynamically register permissions with Laravel's Gate
+            foreach ($permissions as $permission) {
+                Gate::define($permission->name, function (User $user) use ($permission) {
+                    return $user->hasPermission($permission);
+                });
+            }
+        }
+
+        Gate::define('like', [LikePolicy::class, 'like']);
+        Gate::define('unlike', [LikePolicy::class, 'unlike']);
+
+        Password::defaults(function () {
+            $rule = Password::min(9);
+
+            return $this->app->isProduction()
+                ? $rule->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised()
+                : $rule;
+        });
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+
     }
 }
