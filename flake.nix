983dab615680d1b77515c3fc875fa00525db5872
Michael Manning <michael@manning390.com>
2024-09-28 19:11:07 -0700

    Initial nixos devshell

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+
+          modules = [
+            {
+              packages = with pkgs; [
+                # App
+                ffmpeg
+                audiowaveform
+
+                # Development
+                phpactor # LSP
+                vscode-langservers-extracted
+              ];
+
+              scripts.art.exec = ''php artisan "$@"'';
+
+              languages.php = {
+                enable = true;
+                package = pkgs.php83;
+                extensions = [
+                  "ctype"
+                  "curl"
+                  "dom"
+                  "fileinfo"
+                  "mbstring"
+                  "openssl"
+                  "pdo"
+                  "session"
+                  "tokenizer"
+                  "xml"
+                ];
+              };
+
+              services.meilisearch = {
+                enable = true;
+              };
+
+              services.mailhog = {
+                enable = true;
+              };
+
+              env.DB_HOST = "localhost";
+              services.mysql = {
+                enable = true;
+                initialDatabases = [{name = "abidan"; }];
+                ensureUsers = [
+                  {
+                    name = "sail";
+                    password = "password";
+                    ensurePermissions = {"abidan.*" = "ALL PRIVILEGES";};
+                  }
+                ];
+              };
+            }
+          ];
+        };
+      });
+  };
+}
